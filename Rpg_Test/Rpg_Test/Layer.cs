bb0c8561b396c7ed7235778a65c2984aa9cf4e72
rooper149 <rooper149@gmail.com>
2014-01-20 11:00:40 -0600

    Map Design

---
+++ Rpg_Test/Rpg_Test/Layer.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Rpg_Test
+{
+    public class Layer
+    {
+        public class TileMap
+        {
+            public List<string> Row;
+            public TileMap()
+            {
+                Row = new List<string>();
+            }
+        }
+
+        public TileMap TileMap;
+        List<Tile> tiles;
+
+        public Layer()
+        {
+
+        }
+
+        public void LoadContent(Vector2 tileDimentions)
+        {
+
+        }
+
+        public void UnloadContent()
+        {
+
+        }
+
+        public void Update(GameTime gameTime)
+        {
+
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+
+        }
+
+
+    }
+}
---
+++ Rpg_Test/Rpg_Test/Tile.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Rpg_Test
+{
+    public class Tile
+    {
+    }
+}
